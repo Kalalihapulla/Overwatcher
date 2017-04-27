a1ab9ce80effbd3e616b851ee964ce9fd5ab3310
Himel Rahman <himel.rahman@metropolia.fi>
2017-04-27 20:46:18 +0300

    Dependencies

---
+++ src/main/java/Controller/Userdata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Izymi
+ */
+@RestController
+@EnableAutoConfiguration
+public class Userdata {
+
+    @RequestMapping("/")
+    String user() {
+        return "lul";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Userdata.class, args);
+    }
+}
