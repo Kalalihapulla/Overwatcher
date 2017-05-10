944f89083f1dc533c4cf9e0f50384a9bd3704963
himelr <rebelhaze@gmail.com>
2017-05-10 17:00:22 +0300

    Fixes

--- src/main/webapp/script/javascript.js
+++ src/main/webapp/script/javascript.js
@@ -262,51 +262,7 @@ $(document).ready(function () {
 
     }
 
-//    function getWinrateGauge(winRate) {
-//
-//        var winRate = winRate;
-//        var winRateOpposite = 100 - winRate;
-//
-//        var chart = c3.generate({
-//            bindto: '#statsboard',
-//            data: {
-//                columns: [
-//                    ['data', winRate],
-//                    ['data2', winRateOpposite]
-//                ],
-//                type: 'gauge',
-//                onclick: function (d, i) {
-//                    console.log("onclick", d, i);
-//                },
-//                onmouseover: function (d, i) {
-//                    console.log("onmouseover", d, i);
-//                },
-//                onmouseout: function (d, i) {
-//                    console.log("onmouseout", d, i);
-//                }
-//            },
-//            gauge: {
-//                min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
-//                max: 100, // 100 is default
-//                units: ' %',
-//                width: 39 // for adjusting arc thickness
-//            },
-//            color: {
-//                pattern: ['#FF0000', '#F97600'], // the three color levels for the percentage values.
-//                threshold: {
-////            unit: 'value', // percentage is default
-////            max: 200, // 100 is default
-//                    values: [50, 100]
-//                }
-//            },
-//            size: {
-//                height: 180
-//            }
-//        });
-//
-//        return chart;
-//    }
-//
+
 
 
 
